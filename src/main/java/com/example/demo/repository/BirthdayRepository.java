e0ac37cf0ba6b2aea3684b34fbd2e5362ccda566
Igor_Kochubey <i.kochubey@petshop.ru>
2021-02-09 19:55:29 +0300

    Use repository

--- src/main/java/com/example/demo/repository/BirthdayRepository.java
+++ src/main/java/com/example/demo/repository/BirthdayRepository.java
@@ -1,11 +1,22 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Birthday;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BirthdayRepository extends JpaRepository<Birthday, Integer> {
-    @Query("select b from Birthday b where b.userId = :userId and b.chatId = :chatId")
-    Birthday findByUserIdAndChatId(@Param("userId") int userId, @Param("chatId") long chatId);
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface BirthdayRepository extends CrudRepository<Birthday, Integer> {
+    Birthday findByUserIdAndChatId(Long userId, long chatId);
+
+    @Override
+    List<Birthday> findAll();
+
+    long countByChatIdAndResponsibleIsTrue(long chatId);
+
+    List<Birthday> findByBirthdayDate(LocalDate birthdayDate);
+
+    List<Birthday> findByChatIdAndResponsibleTrue(Long chatId);
 }
