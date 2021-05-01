b13d7d6c0488171b477d0a4b09064ee4e04bf677
Igor_Kochubey <i.kochubey@petshop.ru>
2021-05-01 23:57:17 +0300

    Use indexes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,10 +2,12 @@ CREATE SEQUENCE birthday_id_seq;
 CREATE TABLE birthday
 (
     id integer NOT NULL DEFAULT nextval('birthday_id_seq'),
-    userId integer NOT NULL,
-    userName text DEFAULT NULL ,
-    chatId integer NOT NULL,
-    birthdayDate date DEFAULT NULL,
+    user_id integer NOT NULL,
+    user_name text DEFAULT NULL ,
+    chat_id integer NOT NULL,
+    birthday_date date DEFAULT NULL,
     responsible boolean NOT NULL,
     PRIMARY KEY (id)
 );
+CREATE UNIQUE INDEX birthday_userChatId ON birthday (user_id, chat_id);
+CREATE UNIQUE INDEX birthday_userBirthday ON birthday (user_id, birthday_date);
